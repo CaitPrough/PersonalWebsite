85e1e7326e1f7576622ce3979765079864497188
caitprough <caitprough@gmail.com>
2025-06-24 17:36:36 -0500

    basic nav

---
+++ personal-website/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about">
+      <h2>About Me</h2>
+      <p>blablabla</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ personal-website/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="intro">
+      <h1>Cait Prough</h1>
+      <p>yadayadayada</p>
+    </div>
+  );
+}
+
+export default Home;
