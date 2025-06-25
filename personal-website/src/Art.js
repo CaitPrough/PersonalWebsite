affed5c989b623b478d95818280ed729c7d9d2e5
caitprough <caitprough@gmail.com>
2025-06-24 21:50:38 -0500

    added text for art page, also spent way too long formatting

--- personal-website/src/Art.js
+++ personal-website/src/Art.js
@@ -3,16 +3,94 @@ import './Art.css';
 
 function Art() {
   return (
-    <div className="about-container">
-      <div className="about-image">
-        <img src="" alt="image to go here" /> 
-      </div>
-      <div className="about-text">
-        <h2>title?</h2>
-        <p>
-          text
-        </p>
-      </div>
+    <div className="art-page">
+        <div className="cmy">
+            <div className="about-image">
+                <img src="" alt="image to go here" /> 
+            </div>
+            <div className="about-text">
+                <h2>Ponca City, Oklahoma (2024)</h2>
+                <p>
+                    This is one of my favorite pieces and was 
+                    piece was created on alleyway wall during 
+                    Sunny Days Mural Festival (2024) as a part 
+                    of the mentorship program. The top left 
+                    corner features the classic "RYB" primary 
+                    colors, and the bottom left features the 
+                    "CMY" colors commonly seen in things like 
+                    printing.
+                </p>
+            </div>
+        </div>
+
+        <div className="cmy">
+            <div className="about-text">
+                <h2>Edmond, Oklahoma (2024)</h2>
+                <p>
+                    This piece was created on a fence panel 
+                    during Sunny Days Mural Festival (2022) 
+                    as a part of the mentorship program. It 
+                    is permanently located near Edmond fine 
+                    arts!
+                </p>
+            </div>
+            <div className="about-image">
+                <img src="" alt="image to go here" /> 
+            </div>
+        </div>
+
+        <div className="cmy">
+            <div className="about-image">
+                <img src="" alt="image to go here" /> 
+            </div>
+            <div className="about-text">
+                <h2>Ponca City, Oklahoma (2024)</h2>
+                <p>
+                    This piece of art was printed on vinyl 
+                    and applied onto a traffic signal box 
+                    in Ponca City, Oklahoma. The Ponca City 
+                    Main Street organization held a competition 
+                    for art to help add art to the public.
+                </p>
+            </div>
+        </div>
+
+        <div className="cmy">
+            <div className="about-text">
+                <h2>Edmond, Oklahoma (2024)</h2>
+                <p>
+                    In middle school I designed and painted this 
+                    mural as a part of my art class. This mural 
+                    can be found painted on the wall in Classen 
+                    School of Advanced Studies Middle School. It 
+                    is made to resemble a color wheel because it 
+                    is located outside an art classroom.
+                </p>
+            </div>
+            <div className="about-image">
+                <img src="" alt="image to go here" /> 
+            </div> 
+        </div>
+
+        <div className="dumpster">
+            <div className="about-image">
+                <img src="" alt="image to go here" /> 
+            </div>
+            <div className="about-text">
+                <h2>Oklahoma City, Oklahoma (2024)</h2>
+                <p>
+                    This piece was created on the side of a 
+                    Scissortail Waste Solutions dumpster 
+                    during Sunny Days Mural Fest in August 
+                    of 2021. The Sunny Days mentorship 
+                    program paired me up with a wonderful 
+                    pair of ladies that helped me develop 
+                    a plan to move forward with my project. 
+                    This work is an actively used as a traveling 
+                    dumpster.
+                </p>
+            </div>  
+        </div>
     </div>
   );
 }
